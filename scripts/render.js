f333d19e32b63aaa21c6efba65b4a7de10ac8146
Mstudent1996 <1000041083@mcdmstudent.dk>
2025-02-27 12:48:34 +0100

    mai lav mobile first

--- scripts/render.js
+++ scripts/render.js
@@ -6,7 +6,53 @@ const profil = await fetchProfiles();
 export const renderProfiles = () => {
   const popover = document.querySelector(".quickview-content");
   const profileContainer = document.querySelector(".profile-container");
-console.log("test")
+
+  /* Swipe */
+  let slidesLength = slides.length - 1
+  let currentimageIndex = 0
+
+  const setaActiveSlide = (index) => {
+    slides.forEach( (slide) => {
+      slide.classList.remove('active')
+    })
+
+    slides[index].classList.add('active')
+  }
+
+  const next = () => {
+    if(currentimageIndex >= slidesLength) {
+      currentimageIndex = 0
+    } else {
+      currentimageIndex += 1
+    }
+
+    setaActiveSlide(currentimageIndex)
+  }
+
+  const like = () => {
+    let likes = JSON.parse(localStorage.getItem ("likes")) || []
+    const userToAdd = users.find ((user) => user.id == currentimageIndex) 
+    const exist = likes.find(user => user.id === userToAdd.id)
+
+    if(!exist){
+      likes.push(userToAdd)
+      localStorage.setItem('likes', JSON.stringify(likes))
+    }
+
+    next()
+  }
+
+  const likeBtn = document.querySelector('.like')
+  const dislikeBtn = document.querySelector('.dislike')
+
+  likeBtn.addEventListener('click', like)
+  dislikeBtn.addEventListener('click', next)
+
+  setaActiveSlide(currentimageIndex)
+
+
+
+  /* Udskrivning */
   if (profileContainer) {
     profil.forEach((profil) => {
       profileContainer.innerHTML = profileTemplate(profil);

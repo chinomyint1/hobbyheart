8da2d9f24717f9d4f53579a2c3ef6c7c92245a6c
Mstudent1996 <1000041083@mcdmstudent.dk>
2025-02-27 09:22:56 +0100

    update.index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-import { menu } from './burgermenu.js';
-
-menu()
-=======
 /* Import til render */
 /* Import til singleprofile */
 import { likes } from "./likes.js";
 /* Import til filter */
+import { menu } from "./burgermenu.js";
+
+menu()
+likes()
 
 
 
 
-const hamburger = document.querySelector('hamburger'); 
-
-const navLinks = document.querySelector('nav-links'); 
-
- 
-
-hamburger.addEventListener('click', () => { 
-
-    navLinks.classList.toggle('active'); 
-
-}); 
->>>>>>> 71f67ffc87ad0086842617f76f967b5681857b49

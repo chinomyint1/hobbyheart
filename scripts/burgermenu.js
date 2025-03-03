618f1898c35e9eaeadc1b324080df279dcbcb315
chinomyint1 <1000041084@mcdmstudent.dk>
2025-03-03 09:13:25 +0100

    Update burgermenu.js

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('#hamburger');
 
 const navLinks = document.querySelector('.nav-links'); 
 
+
  export function menu() {
     hamburger.addEventListener('click', () => { 
 

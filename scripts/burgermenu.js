5fbcbe3054427e6bb4f34157520f99fa00db681e
Mstudent1996 <1000041083@mcdmstudent.dk>
2025-03-04 11:10:30 +0100

    update burgermenu.js

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -1,18 +1,18 @@
+/* Opretter variabler */
 const hamburger = document.querySelector('#hamburger'); 
-
 const navLinks = document.querySelector('.nav-links'); 
 
-
+/* Funktion til menu */
  export function menu() {
-    hamburger.addEventListener('click', () => { 
+    hamburger.addEventListener('click', () => { /* Click event */
 
-   navLinks.classList.toggle('active'); 
+   navLinks.classList.toggle('active'); /* Viser dropdown menu */
    }); 
 
-window.addEventListener('click', (e) => {
+window.addEventListener('click', (e) => { /* Tilføjer click event på hele vinduet */
       
-   if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) { 
-       navLinks.classList.remove('active');
+   if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) { /* Tjekker om det der bliver klikket på er inden for navLinks eller er hamburger*/
+       navLinks.classList.remove('active'); /* Hvis ikke klikket finder sted på navLinks eller hamburger, fjernes active og menuen lukkes */
        
  }
     });
